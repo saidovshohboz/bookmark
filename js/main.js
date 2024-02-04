ff22e9a603d8419fc1018b45084405b0765acecb
saidovshohboz <shohbozsaidovv@gmail.com>
2024-02-04 19:29:26 +0500

    Make feature tabs work

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let elsTabsItem = document.querySelectorAll('.tabs__item');
+let elsTabsPanel = document.querySelectorAll('.tabs__panel');
+let elsTabLink = document.querySelectorAll('.js-tab-link');
+
+function deactivateTabItems () {
+  elsTabsItem.forEach(function (elTabsItem) {
+    elTabsItem.classList.remove('tabs__item--active');
+  });
+}
+
+function deactivateTabPanels () {
+  elsTabsPanel.forEach(function (elTabsPanel) {
+    elTabsPanel.classList.remove('tabs__panel--active');
+  });
+}
+
+elsTabLink.forEach(function (elTabLink) {
+  elTabLink.addEventListener('click', function (evt) {
+    // Prevent page move
+    evt.preventDefault();
+
+    // Remove active class from tabs__item elements
+    deactivateTabItems();
+
+    // Add active class to current tabs__item
+    elTabLink.parentElement.classList.add('tabs__item--active');
+
+    // Remove active class from tabs__panel elements
+    deactivateTabPanels();
+
+    // Show active tab panel
+    // let elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
+    let elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
+    elTargetPanel.classList.add('tabs__panel--active');
+  });
+});
